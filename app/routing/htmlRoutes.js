a8f81736c6e8d8673897b3183bbe9fbd88fff88a
craigz123 <czimbler60@gmail.com>
2019-04-01 11:49:17 -0400

    lot's of changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const router = express.Router();
+
 const path = require('path');
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + './../public/home.html'))
-})
+module.exports = function(app){
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname + '/../public/survey.html'))
+    })
+    
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/survey.html'))
+    })
+}
 
-router.get('/survey', function(req, res){
-    res.sendFile(path.join(__dirname + './../public/survey.html'))
-})
 
 
 
 
-module.exports = router;
+
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 //required dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
-var fs = require("fs");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//adding application routes
-var friends = require('./app/data/friends.js');
-var htmlRoutes = require("./app/routing/htmlRoutes.js");
-var apiRoutes = require("./app/routing/apiRoutes.js");
-app.use(htmlRoutes);
-app.use(apiRoutes);
-
 //middleware for parsing incoming request bodies
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//adding application routes
+// var friends = require('./app/data/friends.js');
+// var htmlRoutes = require("./app/routing/htmlRoutes.js");
+// var apiRoutes = require("./app/routing/apiRoutes.js");
+// app.use(htmlRoutes);
+// app.use(apiRoutes);
+require("./app/routing/htmlRoutes")(app);
+
+
 
 //start listening to port
 app.listen(PORT, function() {
